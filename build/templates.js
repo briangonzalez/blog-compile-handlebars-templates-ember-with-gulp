f71709839326a5b0538bfd050988ffe02c12d2b3
Brian Gonzalez <me@briangonzalez.org>
2014-11-01 08:28:58 -0700

    first commit

---
+++ build/templates.js
@@ -0,0 +1,40 @@
+window["Ember"] = window["Ember"] || {};
+window["Ember"]["TEMPLATES"] = window["Ember"]["TEMPLATES"] || {};
+window["Ember"]["TEMPLATES"]["bar"] = Ember.Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = '';
+  data.buffer.push("<h1>Hello from bar</h1>\n");
+  stack1 = helpers._triageMustache.call(depth0, "someContent", {"name":"_triageMustache","hash":{},"hashTypes":{},"hashContexts":{},"types":["ID"],"contexts":[depth0],"data":data});
+  if (stack1 != null) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+},"useData":true});
+window["Ember"] = window["Ember"] || {};
+window["Ember"]["TEMPLATES"] = window["Ember"]["TEMPLATES"] || {};
+window["Ember"]["TEMPLATES"]["baz"] = Ember.Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = '';
+  data.buffer.push("<h1>Hello from baz</h1>\n");
+  stack1 = helpers._triageMustache.call(depth0, "someContent", {"name":"_triageMustache","hash":{},"hashTypes":{},"hashContexts":{},"types":["ID"],"contexts":[depth0],"data":data});
+  if (stack1 != null) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+},"useData":true});
+window["Ember"] = window["Ember"] || {};
+window["Ember"]["TEMPLATES"] = window["Ember"]["TEMPLATES"] || {};
+window["Ember"]["TEMPLATES"]["foo"] = Ember.Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = '';
+  data.buffer.push("<h1>Hello from foo</h1>\n");
+  stack1 = helpers._triageMustache.call(depth0, "someContent", {"name":"_triageMustache","hash":{},"hashTypes":{},"hashContexts":{},"types":["ID"],"contexts":[depth0],"data":data});
+  if (stack1 != null) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+},"useData":true});
+window["Ember"] = window["Ember"] || {};
+window["Ember"]["TEMPLATES"] = window["Ember"]["TEMPLATES"] || {};
+window["Ember"]["TEMPLATES"]["nested/qux"] = Ember.Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = '';
+  data.buffer.push("<h1>Hello from qux</h1>\n");
+  stack1 = helpers._triageMustache.call(depth0, "someContent", {"name":"_triageMustache","hash":{},"hashTypes":{},"hashContexts":{},"types":["ID"],"contexts":[depth0],"data":data});
+  if (stack1 != null) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+},"useData":true});
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var streamify   = require('gulp-streamify');
+var declare     = require('gulp-declare');
+var wrap        = require('gulp-wrap');
+var tap         = require('gulp-tap');
+var compiler    = require('ember-template-compiler');
+
+var templateDir = 'templates';
+
+gulp.task('compile-handlebars', function(){
+
+  // First - Grab all of the templates nested within our ./templates dir.
+  return gulp.src(templateDir + '/**/*.hbs')
+
+    // Second - Tap into the stream of files, compiling the template.
+    // The second argument (false) of precompile says return a string
+    // and not a function.
+    .pipe(tap(function(file, t) {
+      var compiled = compiler.precompile(file.contents.toString(), false);
+      file.contents = new Buffer(compiled);
+    }))
+
+    // Third - Since the tap step above output a string, we need to make
+    // sure we wrap the output in the Ember.Handlebars.template function.
+    .pipe(wrap('Ember.Handlebars.template(<%= contents %>)'))
+
+    // Fourth - Create an Ember.TEMPLATES object and attach it to the window.
+    .pipe(declare({
+      namespace: 'Ember.TEMPLATES',
+      root: 'window',
+      processName: function(filePath) {
+        var id = filePath.split(templateDir + '/')[1].replace('.hbs', '');
+        return id;
+      }
+    }))
+
+    // Fifth - Concatenate the templates and output to build dir.
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('./build'));
+
+});
